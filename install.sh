a6495ff309cf322edf2e8efc22bff4978dbffeb4
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-11-13 20:39:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ fi
 # Install utilities
 #
 echo "Install utilities (htop, zip, curl, etc.)"
-apt-get install -y htop unzip zip curl
+apt-get install -y htop unzip zip curl xz-utils
 
 #
 # Create user with ssh key
