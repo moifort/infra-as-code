447edcdc4f6c365846bff9e8cd3e34ea32b2c225
Thibaut Mottet <thibaut.mottet@gmail.com>
2018-10-13 15:23:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ sh get-docker.sh
 # Install docker-compose
 #
 echo "Install docker-compose"
-curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # non-root access
